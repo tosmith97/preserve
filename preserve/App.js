c3e44c6726366baf4b116ddaf8095e9c734fd069
Shakka <tosmith1997@gmail.com>
2017-07-15 14:24:49 -0700

    Preloaded testimoniesnow appear as markers on map, button moved to top of screen

--- preserve/App.js
+++ preserve/App.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { Components } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView } from 'expo';
+import { Button } from 'react-native-elements';
 import testimony from './testimony.json'
 
 export default class App extends React.Component {
+  
+  state = {
+  markers: []
+  }
+
+  _handlePress = (event)=>{
+    console.log("got clicked1");
+    this.setState({
+      markers: [... this.state.markers,
+            {
+            coordinate:{latitude: 37.78825,
+            longitude: -122.4324},
+            title:"title",
+            description:"description"
+          }
+         ]
+    });
+  }
+  
   render() {
 
-    // return(
-    //   <View style={styles.container}>   
-    //     {testimony["testimonials"].map((item) => (
-    //       <View>
-    //         <Text>{item.lat}</Text>
-    //         <Text>{item.long}</Text>
-    //         <Text>{item.testimony}</Text>
-    //         <Text>{"\n"}</Text>
-    //       </View>
-    //   ))}
-
-    //   </View>
-
-    // )
-
-
     return (
-      //<View style={styles.container}>  
-      <Expo.MapView
-        style={{flex: 1}}
+      <View style={styles.container}>  
+
+      <MapView
+        style={styles.map}
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
@@ -33,27 +39,53 @@ export default class App extends React.Component {
           longitudeDelta: 0.0421,
         }}
       >
+
       {testimony["testimonials"].map((item) => {
-        console.log(item)
-          return <Expo.MapView.Marker
-              //key={marker.key}
+          return <MapView.Marker
               coordinate={{
                 latitude: item.lat,
                 longitude: item.long
               }}
-              //pinColor={marker.color}
           />
       })}
-      </Expo.MapView>
-      //</View>
+
+      {this.state.markers.map(function(element){
+      return <MapView.Marker
+        {...element}
+      />
+      })}
+
+      </MapView>
+
+       <Button
+          raised
+          icon={{name: 'home', size: 32}}
+          buttonStyle={styles.button}
+          textStyle={{textAlign: 'center'}}
+          title={`Add checkpoint`}
+          onPress={this._handlePress}
+        />
+        
+      </View>
     );
   }
 }
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  button: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    borderRadius: 10,
+    top:10,
+    position: 'relative',
+    zIndex:1
+  }
 });
