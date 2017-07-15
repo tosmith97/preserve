f37627f43a48a4f9d646dddeb49ae1d6355b1ac7
Shakka <tosmith1997@gmail.com>
2017-07-15 16:07:19 -0700

    saves info when button clicked, added modal

--- preserve/App.js
+++ preserve/App.js
@@ -1,18 +1,63 @@
 import React from 'react';
 import { Components } from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TouchableOpacity, TextInput} from 'react-native';
 import { MapView } from 'expo';
 import { Button } from 'react-native-elements';
 import testimony from './testimony.json'
+import Modal from 'react-native-modal'
 
 export default class App extends React.Component {
   
   state = {
-  markers: []
+    markers: [],
+    modalVisible: false,
+    placeName: '',
+    testimony: '', 
+    addr: ''
   }
+
+  _showModal = () => this.setState({ isModalVisible: true })
+  _hideModal = () => this.setState({ isModalVisible: false })
+
+  _renderModalContent = () => (
+    <View style={styles.modalContent}>
+      <Text style={{height: 40}}>Add testimony</Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Name of Place" 
+        returnKeyLabel = {"placeName"}
+        onChangeText={(text) => this.setState({placeName : text})}
+        />
+      <TextInput 
+        style={styles.input}
+        placeholder="Address" 
+        returnKeyLabel = {"addr"}
+        onChangeText={(text) => this.setState({addr: text})}
+        />
+      <TextInput 
+        style={styles.input} 
+        placeholder="Personal Testimony" 
+        returnKeyLabel = {"testimony"}
+        onChangeText={(text) => this.setState({testimony: text})}
+        />
+
+      {this._renderButton('Submit', this._submitTestimony)}
+      {this._renderButton('Cancel', this._hideModal)}
+    </View>
+  );
+
+  _renderButton = (text, onPress) => (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.modalButton}>
+        <Text>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   _handlePress = (event)=>{
     console.log("got clicked1");
+    this._showModal(true)
     this.setState({
       markers: [... this.state.markers,
             {
@@ -24,11 +69,33 @@ export default class App extends React.Component {
          ]
     });
   }
+
+  _submitTestimony = (event)=>{
+    console.log("Testimony submitted");
+    console.log(this.state.placeName)
+    console.log(this.state.addr)
+    console.log(this.state.testimony)
+    this._showModal(false)
+    this.setState({
+      markers: [... this.state.markers,
+            {
+            coordinate:{latitude: 37.78825,
+            longitude: -122.4324},
+            title: this.state.placeName,
+            description: this.state.testimony
+          }
+         ]
+    });
+  } 
   
   render() {
 
     return (
       <View style={styles.container}>  
+
+      <Modal isVisible={this.state.isModalVisible}>
+          {this._renderModalContent()}
+      </Modal>
 
       <MapView
         style={styles.map}
@@ -87,5 +154,25 @@ const styles = StyleSheet.create({
     top:10,
     position: 'relative',
     zIndex:1
+  },
+  modalButton: {
+    backgroundColor: 'lightblue',
+    padding: 12,
+    margin: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    //alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  input: {
+    textAlign: 'center'
   }
 });
