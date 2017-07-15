65e9400766b42cfbfd084d0c1e8c2601af4a6f15
Shakka <tosmith1997@gmail.com>
2017-07-15 13:21:45 -0700

    json -> markers

--- preserve/App.js
+++ preserve/App.js
@@ -1,18 +1,54 @@
 import React from 'react';
+import { Components } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
+import testimony from './testimony.json'
 
 export default class App extends React.Component {
   render() {
+
+    // return(
+    //   <View style={styles.container}>   
+    //     {testimony["testimonials"].map((item) => (
+    //       <View>
+    //         <Text>{item.lat}</Text>
+    //         <Text>{item.long}</Text>
+    //         <Text>{item.testimony}</Text>
+    //         <Text>{"\n"}</Text>
+    //       </View>
+    //   ))}
+
+    //   </View>
+
+    // )
+
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      //<View style={styles.container}>  
+      <Expo.MapView
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+      {testimony["testimonials"].map((item) => {
+        console.log(item)
+          return <Expo.MapView.Marker
+              //key={marker.key}
+              coordinate={{
+                latitude: item.lat,
+                longitude: item.long
+              }}
+              //pinColor={marker.color}
+          />
+      })}
+      </Expo.MapView>
+      //</View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
